2424df492679a5e3f535218625d78738c4822103
fuzi233 <fuzi1fuzi1@163.com>
2025-05-22 21:26:55 +0800

    Add Dataset.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
         "--benchmark",
         type=str,
         default="math",
-        choices=["math", "drop", "gsm8k", "hotpotqa", "humaneval", "mbpp", "swebench"],
+        choices=["math", "drop", "gsm8k", "hotpotqa", "humaneval", "mbpp", "swebench","mmlu_pro"],
         help="Benchmark to run (default: math)",
     )
 
