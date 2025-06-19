da896b51be81e2ce658cd74b0ed9d84120a0acc1
RuishanFang <22015012@zju.edu.cn>
2025-06-19 22:50:06 +0800

    opt PythonReplTool name

--- benchmark/src/tools/__init__.py
+++ benchmark/src/tools/__init__.py
@@ -8,7 +8,7 @@ from .browser_tool import BrowserTool
 from .document_analysis_tool import DocumentAnalysisTool
 from .shell_tool import ShellTool
 from .search_api_tool import SearchApiTool
-from .python_execute_tool import PythonREPLTool
+from .python_execute_tool import PythonReplTool
 from .android_tool import AndroidTool
 
 __all__ = ["ToolManager", "ToolSelector"] 
